51e89f738f6ec1c105b138169354f0aa2830fa60
Joshua Zenn <wordman05@gmail.com>
2014-10-29 10:11:17 -0700

    Create settings.php
    Added settings for title, caption, and number of posts to show on home page

---
+++ settings.php
@@ -0,0 +1,5 @@
+<?php
+$cf_title = 'Blog Title'; //Title of your blog, shown on all pages
+$cf_caption = 'Blog caption."; //Caption to go underneath title, shown on all pages
+$cf_hp_visposts = 5; //number of posts to show on homepage
+?>
