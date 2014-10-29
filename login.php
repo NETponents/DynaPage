41a28a14247be4cc3e900235f1a39afbe9cb2715
Joshua Zenn <wordman05@gmail.com>
2014-10-29 09:55:50 -0700

    Create login.php
    Created base login info file.

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+//Fill out this file with your own DB info before attepting to run setup
+$db_hostname = 'localhost';
+$db_database = 'database';
+$db_username = 'username';
+$db_password = 'password';
+?>
