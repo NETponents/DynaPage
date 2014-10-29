911f3267777735057a66013868e77711e833733c
Joshua Zenn <wordman05@gmail.com>
2014-10-29 09:57:13 -0700

    Delete login.php
    Added in wrong branch. Copying to master branch

--- login.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Fill out this file with your own DB info before attepting to run setup
-$db_hostname = 'localhost';
-$db_database = 'database';
-$db_username = 'username';
-$db_password = 'password';
-?>
