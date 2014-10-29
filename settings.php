93e326f8f37481d0b79360ca1795198b487c7fa2
Joshua Zenn <wordman05@gmail.com>
2014-10-29 10:37:37 -0700

    Update settings.php
    Fixed missing ' and added legal content entry

--- settings.php
+++ settings.php
@@ -1,5 +1,6 @@
 <?php
 $cf_title = 'Blog Title'; //Title of your blog, shown on all pages
-$cf_caption = 'Blog caption."; //Caption to go underneath title, shown on all pages
+$cf_caption = 'Blog caption.'; //Caption to go underneath title, shown on all pages
+$cf_legal = 'Copyright 2014'; //Copyright statement shown in footer on all pages
 $cf_hp_visposts = 5; //number of posts to show on homepage
 ?>
