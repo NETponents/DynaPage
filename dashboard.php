4675f2bc01e831ceaad940606990bed32b35d928
Joshua Zenn <wordman05@gmail.com>
2014-11-03 12:49:46 -0500

    Update dashboard.php
    Added base admin page with slots for addon packages. Login resources diverted to auth.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,48 @@
 <?php
 require_once auth.php;
 require_once styles.php;
-require_once sql.php;
+require_once settings.php;
+require_once sqldata.php;
+if($isloggedin())
+{
+  echo "<html><body>FORBIDDEN</body></html>";
+}
+else if($isadmin())
+{
+  echo "<html><body>FORBIDDEN</body></html>";
+}
+else if($canaccesswebconsole())
+{
+  echo "<html><body>FORBIDDEN</body></html>";
+}
+else
+{
+//start loading page
+echo "<html><head>";
+echo '<LINK href=\"' . $st_home . '\" rel="stylesheet" type="text/css">';
+//This is the place to add google analytics code
+//echo
+
+echo "</header><body>";
+if(!$st_bodyimage)
+{
+  echo '<body>';
+}
+else
+{
+  echo '<body style=\"background-image:url(' . $st_bodyimage . ')\">';
+}
+echo "<h1>" . $cf_title . "</h1>";
+echo "<h2>" . $cf_caption . "</h2>";
+echo '<hr />';
+echo '<br /><br />';
+
+//start admin content
+
+//end admin content
+echo '<hr />';
+echo $cf_legal . '<br />';
+echo 'Developed with <a href=\"github.com/ARMmaster17/DynaPage/\">DynaPage</a>';
+echo "</body></html>";
+}
 ?>
