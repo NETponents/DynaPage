95b619ae0d8d819305c524754520d02e1870fc66
Joshua Zenn <wordman05@gmail.com>
2014-11-19 21:28:40 -0500

    Update settings.php
    Added security section and entry for login component to admin dashboard.

--- settings.php
+++ settings.php
@@ -2,5 +2,8 @@
 $cf_title = 'Blog Title'; //Title of your blog, shown on all pages
 $cf_caption = 'Blog caption.'; //Caption to go underneath title, shown on all pages
 $cf_legal = 'Copyright 2014'; //Copyright statement shown in footer on all pages
-$cf_hp_visposts = 5; //number of posts to show on homepage
+$cf_hp_visposts = 5; //Number of posts to show on homepage
+
+//Security
+$sc_adminPanel = 1; //Maximum access level to the site dashboard
 ?>
