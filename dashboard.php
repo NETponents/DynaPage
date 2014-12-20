56fc9d16026ec0dd789c5ac12eb1a1d46395a6c7
Joshua Zenn <wordman05@gmail.com>
2014-12-20 14:48:14 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -39,7 +39,10 @@ $dir = $cf_rooturl . "/admin/modules/";
 $dh  = opendir($dir);
 while (false !== ($filename = readdir($dh)))
 {
-    echo "<tr><td><iframe src=\"" . $dir . $filename . "\"></iframe></td></tr>";
+    //echo "<tr><td><iframe src=\"" . $dir . $filename . "\"></iframe></td></tr>";
+    echo "<tr><td>";
+    include $dir . $filename;
+    echo "</td></tr>";
 }
 echo "</table>";
 //end admin content
