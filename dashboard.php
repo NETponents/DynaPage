9ae25b3c342fe2873a44433ca7a92d7e3fe21fa6
Joshua Zenn <wordman05@gmail.com>
2014-12-20 14:25:51 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,8 @@
 <?php
-require_once 'cklogin.php';
-require_once 'styles.php';
-require_once 'settings.php';
-require_once 'sqldata.php';
+require 'cklogin.php';
+require 'styles.php';
+require 'settings.php';
+require 'sqldata.php';
 if(isloggedin() != 0)
 {
   echo "<html><body>FORBIDDEN</body></html>";
