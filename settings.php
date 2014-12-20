d436d95058c0605acdafc3cf98f61edc19a52385
Joshua Zenn <wordman05@gmail.com>
2014-12-20 14:33:15 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,6 +3,7 @@ $cf_title = 'Blog Title'; //Title of your blog, shown on all pages
 $cf_caption = 'Blog caption.'; //Caption to go underneath title, shown on all pages
 $cf_legal = 'Copyright 2014'; //Copyright statement shown in footer on all pages
 $cf_hp_visposts = 5; //Number of posts to show on homepage
+$cf_rooturl = 'dynapagetest.netii.net' //url of your website
 
 //Security
 $sc_adminPanel = 1; //Maximum access level to the site dashboard
