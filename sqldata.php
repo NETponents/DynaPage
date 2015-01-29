f40d9bb596346883e4770463a1fdec5f103f0dbe
Joshua Zenn <wordman05@gmail.com>
2015-01-29 10:43:23 -0500

    Update sqldata.php

--- sqldata.php
+++ sqldata.php
@@ -1,6 +1,6 @@
 <?php
 //Fill out this file with your own DB info before attepting to run setup
-$db_hostname = 'nv8sklzebb.database.windows.net';
+$db_hostname = 'nv8sklzebb.database.windows.net,1433';
 $db_database = 'roamingrambo_db';
 $db_username = 'zennjos';
 $db_password = '@story345';
