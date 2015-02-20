d2ea0a88c295fd569ca4495954626c564d69e95f
Joshua Zenn <wordman05@gmail.com>
2015-02-20 09:05:07 -0500

    Update sqldata.php

--- sqldata.php
+++ sqldata.php
@@ -2,6 +2,6 @@
 //Fill out this file with your own DB info before attepting to run setup
 $db_hostname = 'tcp:fibizqzynz.database.windows.net,1433';
 $db_database = 'dynapagA1pgUGAAz';
-$db_username = 'zennjos';
+$db_username = 'zennjos@fibizqzynz';
 $db_password = 'G@meking14';
 ?>
