1c973825f7e9d16ae744b9947c15047a50522842
Joshua Zenn <wordman05@gmail.com>
2015-02-21 15:02:14 -0500

    Update process.php
    Added base XML insertion code

--- process.php
+++ process.php
@@ -1,1 +1,14 @@
+<html>
+<body>
+<p>Please wait...</p>
+<?php
+$text = '<post>';
+$num_bytes = insert_into_file($content_path . '/index.xml', '<posts>', $text, true);
 
+function insert_into_file($file_path, $insert_marker, $textx, $after = true)
+{ 
+  $contents = file_get_contents($file_path); 
+  $new_contents = preg_replace($insert_marker,($after) ? '$0' . $textx : $textx . '$0', $contents); 
+  return file_put_contents($file_path, $new_contents); 
+} 
+?>
