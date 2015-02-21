9b202a8396b833f673c652e7ea51935476ea24f8
Joshua Zenn <wordman05@gmail.com>
2015-02-21 11:02:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ else
 }
 
 //start posts
-$postdata = simple_xml_loadfile('/posts/index.xml');
+//$postdata = simple_xml_loadfile('/posts/index.xml');
 //$numposts = count($postdata);
 //$startindex = $numposts - 1;
 $rows = $cf_hp_visposts;
