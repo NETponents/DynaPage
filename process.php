ccd01304858b8362f2f6a96ca768e76a328ebfe3
Joshua Zenn <wordman05@gmail.com>
2015-02-21 15:09:33 -0500

    Update process.php

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
 <body>
 <p>Please wait...</p>
 <?php
-$text = '<post>';
+$text = '<post><title>' . $_POST['title'] . '</title><content>' . /*find last known post*/ . '</content><data>' . /*same as previous tag*/ . '</data><tag>' . /*same*/ . '</tag></post>';
 $num_bytes = insert_into_file($content_path . '/index.xml', '<posts>', $text, true);
 
 function insert_into_file($file_path, $insert_marker, $textx, $after = true)
