57b40b2cda68ef1b047950ca28af24bf9d78b421
Joshua Zenn <wordman05@gmail.com>
2015-02-21 14:52:04 -0500

    Update write.php
    Added footer information

--- write.php
+++ write.php
@@ -37,5 +37,22 @@ else
   Content: <input type="text" name="content"><br>
   <input type="submit">
 </form>
-</body>
-</html>
+<?php
+//begin footer of visible page
+echo '<hr />';
+echo $cf_legal . '<br />';
+echo 'Developed with <a href=\"github.com/ARMmaster17/DynaPage/\">DynaPage</a>';
+
+//end body
+echo '</body>';
+
+//open footer
+echo '<footer>';
+//add footer here. Ex: advanced page load timing JS snippet
+echo '</footer>';
+
+//close HTML tag
+echo '</html>';
+
+//end page
+?>
