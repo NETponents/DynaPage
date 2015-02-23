50c2b17f56f1dae64a19b0dba800209ea285c128
Joshua Zenn <wordman05@gmail.com>
2015-02-23 10:03:22 -0500

    Update process.php
    Added tag identification

--- process.php
+++ process.php
@@ -2,7 +2,9 @@
 <body>
 <p>Please wait...</p>
 <?php
-$text = '<post><title>' . $_POST['title'] . '</title><content>' . /*find last known post*/ . '</content><data>' . /*same as previous tag*/ . '</data><tag>' . /*same*/ . '</tag></post>';
+$postdata = simplexml_load_file('posts/index.html');
+$tag = $postdata->post[count($postdata->post) - 1]->ntag + 1;
+$text = '<post><title>' . $_POST['title'] . '</title><content>' . $tag . '.txt' . '</content><data>' . $tag . '.xml' . '</data><tag>' . $tag . '</tag></post>';
 $num_bytes = insert_into_file($content_path . '/index.xml', '<posts>', $text, true);
 
 function insert_into_file($file_path, $insert_marker, $textx, $after = true)
