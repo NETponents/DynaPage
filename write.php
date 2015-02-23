ba43ea7e2c45419ef98428be9484afd74f98cfcc
Joshua Zenn <wordman05@gmail.com>
2015-02-23 12:56:05 -0500

    Update write.php
    Fixed bad HTML tag on button

--- write.php
+++ write.php
@@ -5,7 +5,7 @@ require_once 'header.php';
 <form action="process.php" method="post">
   Title: <input type="text" name="title"><br>
   Content: <input type="text" name="content"><br>
-  <input type="submit" text="Post">
+  <input type="submit" value="Post">
 </form>
 <?php
 //begin footer of visible page
