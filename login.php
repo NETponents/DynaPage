e24bf2ea5f25baf60049c1d56be2a4e060ec1751
Joshua Zenn <wordman05@gmail.com>
2015-04-15 13:49:58 -0400

    Update login.php

--- login.php
+++ login.php
@@ -2,8 +2,16 @@
 require_once 'header.php';
 ?>
 <h3>Sign in</h3>
-<form action='' method='POST'>
-
+<form action="action_page.php" method="POST">
+<p>Username: </p>
+<input type="text" name="uname">
+<br />
+<p>Password:</p> </p>
+<input type="password" name="pword">
+<br />
+<br />
+<hr />
+<input type="submit" value="Log in">
 </form>
 <?php
 require_once 'footer.php';
