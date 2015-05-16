863d8111004e3db50e6c69a0b62b202c7b04322f
Joshua Zenn <wordman05@gmail.com>
2015-05-16 14:47:33 -0400

    Update footer.php

--- design/footer.php
+++ design/footer.php
@@ -1,10 +1,10 @@
 <?php
 //begin footer of visible page 
 require_once './admin/settings.php';
-echo '<hr />'; 
-echo $cf_legal . '<br />'; 
-echo 'Developed with <a href=\'github.com/ARMmaster17/DynaPage/\'>DynaPage</a>'; 
-
+echo '<div class=\"row\"><div class=\"col-sm-12\">'; 
+echo $cf_legal; 
+echo '<br />Developed with <a href=\'github.com/ARMmaster17/DynaPage/\'>DynaPage</a>'; 
+echo '</div></div></div>';
 //end body 
 echo '</body>'; 
 
