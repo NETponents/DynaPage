6025da846601d6485eebeeb6df836ccf73bb4fc1
Joshua Zenn <wordman05@gmail.com>
2015-05-16 14:36:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<header>
-    <title>Thinkit - Official Website</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-</header>
-<body>
 <?php
 //Header design file with page name and CSS stuff
 define( 'SCRIPT_ROOT', 'http://localhost/DynaPage' );
@@ -57,4 +48,3 @@ else
 //Footer design file
 require_once './design/footer.php';
 ?>
-</body>
