51dae63a878297202cc636821a15ba4ee9505a26
Joshua Zenn <wordman05@gmail.com>
2015-05-16 14:42:53 -0400

    Update header.php

--- design/header.php
+++ design/header.php
@@ -12,8 +12,14 @@ echo '<header>';
 echo '<LINK href=\"' . $st_home . '\" rel="stylesheet" type="text/css">';
 echo '<title>' . $cf_title . '- Built with DynaPage</title>';
 //add any header information you need such as Google Analytics tracking
+?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<?php
 echo '</header>';
-
 //start body
 if(!$st_bodyimage)
 {
@@ -23,16 +29,50 @@ else
 {
   echo '<body style=\"background-image:url(' . $st_bodyimage . ')\">';
 }
+?>
+
+<nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+         </button>
+         <a class="navbar-brand" href="./index.html"><?php echo $cf_title; ?></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <!--<li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="./products.html">Products<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Submenu 1-1</a></li>
+                <li><a href="#">Submenu 1-2</a></li>
+                <li><a href="#">Submenu 1-3</a></li>
+              </ul>
+           </li>-->
+           <li><a href="./index.php">Home</a></li>
+          </ul>
+         <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="jumbotron">
+<?php
 if(!$st_headerimage)
 {
   echo "<h1>" . $cf_title . "</h1>";
   echo "<h2>" . $cf_caption . "</h2>";
-  echo '<hr />';
-  echo '<br /><br />';
+  echo '</div>';
 }
 else
 {
   //TODO: finish call to create header image from media source listed in settings.php
   //echo "<img src=\"" . $st_headerimage . ""\" alt="Mountain View" style="width:304px;height:228px">"
+  echo '</div>';
 }
 ?>
