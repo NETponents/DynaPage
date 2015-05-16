d8642899a4ebc909fdf466d1acd141ea534d6cc0
Joshua Zenn <wordman05@gmail.com>
2015-05-16 13:08:14 -0400

    Update footer.php

--- design/footer.php
+++ design/footer.php
@@ -1,13 +1,13 @@
 //begin footer of visible page 
 //require_once './admin/settings.php';
-echo '<hr />'; 
+//echo '<hr />'; 
 //echo $cf_legal . '<br />'; 
-echo 'Developed with <a href=\'github.com/ARMmaster17/DynaPage/\'>DynaPage</a>'; 
+//echo 'Developed with <a href=\'github.com/ARMmaster17/DynaPage/\'>DynaPage</a>'; 
 
 //end body 
-echo '</body>'; 
+//echo '</body>'; 
 
 //open footer 
-echo '<footer>'; 
+//echo '<footer>'; 
 //add footer here. Ex: advanced page load timing JS snippet 
-echo '</footer>';
+//echo '</footer>';
